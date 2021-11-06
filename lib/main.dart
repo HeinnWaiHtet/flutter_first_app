c653e2c79d9f734082ba4278facd2d7f3dc265bc
Hein Wai Htet <cuheinwaihtet@gmail.com>
2021-11-06 17:57:41 +0630

    adding Contianer Widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,40 +17,34 @@ class MyApp extends StatelessWidget{
         appBar: AppBar(
           title: Text(
             "Flutter",
-            style: TextStyle(color: Colors.amber[400]),
-            ),),
-          body: RichText(
-            text: TextSpan(
-              text: "Password",
-              style: TextStyle(
-                color: Colors.green[200],
-                fontWeight: FontWeight.bold,
-                fontSize: 21),
-              children: const [
-                TextSpan(
-                  text: "Forget Password",
-                   style: TextStyle(
-                     wordSpacing: 2,
-                     color: Colors.black12,
-                     fontSize: 13,
-                     fontStyle:FontStyle.italic ,
-                     shadows: [
-                       Shadow(
-                       color: Colors.grey,
-                       offset: Offset(12,15),
-                       blurRadius: 5.5,
-                     )
-                     ])
-                     ),
-                TextSpan(
-                  text: "Another Text",
-                  style: TextStyle(
-                    fontSize: 8,
-                    letterSpacing:1, 
-                    color: Colors.amberAccent),
-                    ),
-              ]),
-            )
+            style: TextStyle(color: Colors.amber[400]),),
+            ),
+          body: Container(
+            decoration: BoxDecoration(
+              border:  Border.all(
+                color: Colors.amberAccent,
+                style: BorderStyle.solid,
+                width: 4)
+                ),
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.only(top: 30,left: 20),
+            width: 200,
+            height: 200,
+            child: const Center(
+              child: Text("Hello Flutter",style: TextStyle(
+                color: Colors.green,
+                letterSpacing: 1,
+                fontSize: 19,
+                shadows: [
+                  Shadow(
+                    color: Colors.black38,
+                    offset: Offset(12,12),
+                    blurRadius: 6.5
+                  ),
+                ] ),
+                ),
+            ),
+          ),
         ),
     );
   }
