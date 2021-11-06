78557bfba5aa34543b3d0fa2a784de640433c6bd
Hein Wai Htet <cuheinwaihtet@gmail.com>
2021-11-06 22:11:24 +0630

    adding OutlinedButton Widget

--- lib/main.dart
+++ lib/main.dart
@@ -20,24 +20,43 @@ class MyApp extends StatelessWidget{
             style: TextStyle(color: Colors.amber[400]),),
             ),
           body: Center(
-            child: TextButton(
-              autofocus: true,
-              child: const Text("TextButton"),
-              onPressed: (){
-              // ignore: avoid_print
-              print('click me');
-            },
-            style: TextButton.styleFrom(
-              backgroundColor: Colors.orangeAccent[200],
-              primary: Colors.white,//For Text Color
-              shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(2))),
-              textStyle :const TextStyle(
-                color: Colors.white,
-                fontSize: 21,
-                letterSpacing: 2,
+            child: Column(
+              children: [
+                TextButton(
+                  autofocus: true,
+                  child: const Text("TextButton"),
+                  onPressed: (){
+                    // ignore: avoid_print
+                    //print('click me');
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.orangeAccent[200],
+                    primary: Colors.white,//For Text Color
+                    shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(2))),
+                    textStyle :const TextStyle(
+                      color: Colors.white,
+                      fontSize: 21,
+                      letterSpacing: 2,
+                       ),
+                      ),
+                ),
+                
+                OutlinedButton(
+                  onPressed: (){}, 
+                  child: const Text("Outlined Button"),
+                  style: OutlinedButton.styleFrom(
+                    primary: Colors.red,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5))
+                    ),
+                    textStyle: const TextStyle(
+                      color: Colors.red,
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic)
+                  ),),
+              ],
             ),
-          )
-        ),
       ),
       ),
     );
