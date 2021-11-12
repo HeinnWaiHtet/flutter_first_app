20f8fedc9b3858cae35393ad9727a8b083e8f7ed
Hein Wai Htet <cuheinwaihtet@gmail.com>
2021-11-13 00:29:03 +0630

    adding form widget and checking form state, dividing textFied widget in new file

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,7 @@ class MyApp extends StatefulWidget{
 }
 
 class MyAppState extends State<MyApp>{
-  int count = 0;
-  String userName = '';
-  TextEditingController textController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
   @override
   // ignore: avoid_renaming_method_parameters
   Widget build(BuildContext buildContext){
@@ -30,71 +28,60 @@ class MyAppState extends State<MyApp>{
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+                  Form(
+                    key: formKey,
+                    child: Column(
+                    children: [
+                      // ignore: avoid_unnecessary_containers
+                      Container(
+                        width: 300,
+                          child : TextFormField(
+                            validator: (value){
+                              if(value == null || value == ''){
+                                return 'Enter Your Email';
+                              }
+                            },
+                            decoration: const InputDecoration(
+                            hintText: 'Enter Your Email',
+                            labelText: 'Email',
+                            labelStyle: TextStyle(color: Colors.cyan),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(15)),
+                            )
+                          ),
+                        ),
+                      ),
 
-              Container(
-                margin: const EdgeInsets.all(10),
-                width: 300,
-                child:TextField(
-                  controller: textController,
-                  decoration: const InputDecoration(
-                    hintText: 'Enter Your Name',
-                    labelText: 'Name',
-                    labelStyle: TextStyle(fontWeight: FontWeight.bold,color: Colors.redAccent,fontSize: 19),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-                  ),
-                ),
-              ),
+                      const SizedBox(height: 20,),
+                      
+                      // ignore: avoid_unnecessary_containers
+                      Container(
+                        width: 300,
+                        child: TextFormField(
+                          validator: (value){
+                            if(value == null || value == ''){
+                              return 'Enter Your Password';
+                            }
+                          },
+                          decoration: const InputDecoration(
+                            hintText: 'Enter Password',
+                            labelText: 'Password',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(15))
+                            )
+                          ),
+                        ),
+                      ),
 
-              Text(
-                "$count",
-                style: const TextStyle(
-                  color: Colors.brown,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  )
-                ),
 
-                const SizedBox(height: 20,),
-
-                Text(
-                userName,
-                style: const TextStyle(
-                  color: Colors.pinkAccent,
-                  fontSize: 30,
-                  fontWeight: FontWeight.normal,
-                  )
-                ),
-
-                const SizedBox(height: 20,),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // ignore: avoid_unnecessary_containers
-                    Container(
-                      child: ElevatedButton(
+                      ElevatedButton(
                         onPressed: (){
-                          setState(() {
-                            count++;
-                          });
-                        }, 
-                        child: const Text('Check Count')) 
-                        ,),
-
-                        const SizedBox(width: 10,),
-
-                    // ignore: avoid_unnecessary_containers
-                    Container(
-                      child: ElevatedButton(
-                        onPressed: (){
-                          setState(() {
-                            userName = textController.text;
-                          });
-                        }, 
-                        child: const Text('Show Name')) 
-                        ,),
-                      ],
-                  ),
+                          formKey.currentState!.validate();
+                      }, 
+                      child: const Text('Click')),
+                    ],
+                   )
+                  )
                 ],
               ),
             )
