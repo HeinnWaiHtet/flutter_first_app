9ebfb4e93fd797e9958178bf275eb85fb5cbb3c1
Hein Wai Htet <cuheinwaihtet@gmail.com>
2021-11-13 01:05:49 +0630

    adding alert dialog and simple dialog select widget

--- lib/main.dart
+++ lib/main.dart
@@ -24,68 +24,71 @@ class MyAppState extends State<MyApp>{
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter Stateful Widget'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-                  Form(
-                    key: formKey,
-                    child: Column(
-                    children: [
-                      // ignore: avoid_unnecessary_containers
-                      Container(
-                        width: 300,
-                          child : TextFormField(
-                            validator: (value){
-                              if(value == null || value == ''){
-                                return 'Enter Your Email';
-                              }
-                            },
-                            decoration: const InputDecoration(
-                            hintText: 'Enter Your Email',
-                            labelText: 'Email',
-                            labelStyle: TextStyle(color: Colors.cyan),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                            )
-                          ),
-                        ),
-                      ),
-
-                      const SizedBox(height: 20,),
-                      
-                      // ignore: avoid_unnecessary_containers
-                      Container(
-                        width: 300,
-                        child: TextFormField(
-                          validator: (value){
-                            if(value == null || value == ''){
-                              return 'Enter Your Password';
-                            }
-                          },
-                          decoration: const InputDecoration(
-                            hintText: 'Enter Password',
-                            labelText: 'Password',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(15))
-                            )
-                          ),
-                        ),
-                      ),
-
-
-                      ElevatedButton(
-                        onPressed: (){
-                          formKey.currentState!.validate();
-                      }, 
-                      child: const Text('Click')),
-                    ],
-                   )
-                  )
-                ],
-              ),
-            )
+        body: const MyHome(),
         ),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget{
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  // ignore: avoid_renaming_method_parameters
+  Widget build(BuildContext buildContext){
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(onPressed: (){
+            showDialog(context: buildContext, builder: (BuildContext context){
+              return AlertDialog(
+                title: const Text(
+                  'Alert Title',
+                   style: TextStyle(
+                     color: Colors.redAccent),
+                     ),
+                content: const Text(
+                  'Hello MG HEIN',
+                   style: TextStyle(
+                     color: Colors.indigoAccent,
+                      fontSize: 21,
+                       fontWeight: FontWeight.normal),
+                       ),
+                actions: [
+                  ElevatedButton(onPressed: (){}, child: const Text('Yes')),
+                  OutlinedButton(onPressed: (){}, child: const Text('No'),)
+                ],
+              );
+            });
+          }, child: const Text('show alert')),
+
+          const SizedBox(height: 15,),
+
+          ElevatedButton(onPressed: (){
+            Future data = showDialog(context: buildContext, builder: (BuildContext context){
+               return SimpleDialog(
+                children: [
+                  SimpleDialogOption(
+                    child: const Text('IPhone', style: TextStyle(color: Colors.pinkAccent,fontSize: 21),),
+                    onPressed: (){
+                      Navigator.pop(context, 'I-Phone');
+                    },
+                  ),
+                  SimpleDialogOption(
+                    child: const Text('Samsumg', style: TextStyle(color: Colors.deepPurple,fontSize: 15),),
+                    onPressed: (){
+                      Navigator.pop(context, 'Samsumng');
+                    },
+                  ),
+                ],
+              );
+            });
+            data.then((value) => print(value));
+          }, child: const Text('show option Dialog'))
+        ],
+      ),
     );
   }
 }
