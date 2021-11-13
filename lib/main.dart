5dbd0d667ab840c6eb390af22e2ce655ce09897e
Hein Wai Htet <cuheinwaihtet@gmail.com>
2021-11-13 12:35:38 +0630

    adding ListView Widget

--- lib/main.dart
+++ lib/main.dart
@@ -20,77 +20,39 @@ class MyAppState extends State<MyApp>{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Flutter State Full Widget",
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Tab Bar'),
-            bottom: const TabBar(
-              indicatorColor: Colors.pinkAccent,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorWeight: 3,
-              tabs: [
-                Tab(child: Text('Home'),icon: Icon(Icons.home_filled),),
-                Tab(child: Text('Profile'),icon: Icon(Icons.person_off_outlined),),
-                Tab(child: Text('about'),icon: Icon(Icons.person_pin_sharp),),
-              ],
-             ),
-            ) ,
-            body: const TabView(),
-            drawer: Drawer(
-              child: Column(
-                children: const [
-                  UserAccountsDrawerHeader(
-                    accountName: Text('chanmay@gmail.com'),
-                    accountEmail: Text('chanmay@gmail.com'),
-                    currentAccountPicture: CircleAvatar(
-                      backgroundImage: AssetImage('assets/chit.jpg'),
-                      child: Text('chit'),
-                      ),
-                    ),
-                ],
-              ),
-            ),
-          ),
-        ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('List View'),),
+          body: const ListData(),
+      ),
     );
   }
 }
 
-class TabView extends StatelessWidget{
-  const TabView({Key? key}) : super(key: key);
+class ListData extends StatelessWidget{
+  const ListData({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return TabBarView(children: [
-      Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Home', style: TextStyle(color: Colors.redAccent,fontSize: 30),),
-            const SizedBox(height: 20,),
-            OutlinedButton(
-              child: const Text('SnackBar'),
-              onPressed: (){
-                SnackBar bar = SnackBar(
-                  content: const Text('Deleted'),
-                  duration: const Duration(seconds: 3),
-                  action: SnackBarAction(label: 'Undo',onPressed: (){},
-                  ),
-                );
-                ScaffoldMessenger.of(context).showSnackBar(bar);
-              },
-            ),
-          ],
-        ),
-      ),
-      const Center(
-        child: Text('User Profile',style: TextStyle(color: Colors.cyanAccent,fontSize: 30),),
-      ),
-      const Center(
-        child: Text('About',style: TextStyle(color: Colors.blueAccent,fontSize: 30),),
-      ),
-    ]);
+    return ListView(
+      children: const [
+        ListTile(title: Text('MacBook'),leading: Icon(Icons.laptop_mac_sharp),focusColor: Colors.green,),
+        ListTile(title: Text('ZenBook'),leading: Icon(Icons.laptop_mac_sharp),hoverColor: Colors.red,),
+        ListTile(title: Text('Hp'),leading: Icon(Icons.laptop_mac_sharp)),
+        ListTile(title: Text('MacBook'),leading: Icon(Icons.laptop_mac_sharp),focusColor: Colors.green,),
+        ListTile(title: Text('ZenBook'),leading: Icon(Icons.laptop_mac_sharp),hoverColor: Colors.red,),
+        ListTile(title: Text('Hp'),leading: Icon(Icons.laptop_mac_sharp)),
+        ListTile(title: Text('MacBook'),leading: Icon(Icons.laptop_mac_sharp),focusColor: Colors.green,),
+        ListTile(title: Text('ZenBook'),leading: Icon(Icons.laptop_mac_sharp),hoverColor: Colors.red,),
+        ListTile(title: Text('Hp'),leading: Icon(Icons.laptop_mac_sharp)),
+        ListTile(title: Text('MacBook'),leading: Icon(Icons.laptop_mac_sharp),focusColor: Colors.green,),
+        ListTile(title: Text('ZenBook'),leading: Icon(Icons.laptop_mac_sharp),hoverColor: Colors.red,),
+        ListTile(title: Text('Hp'),leading: Icon(Icons.laptop_mac_sharp)),
+        ListTile(title: Text('MacBook'),leading: Icon(Icons.laptop_mac_sharp),focusColor: Colors.green,),
+        ListTile(title: Text('ZenBook'),leading: Icon(Icons.laptop_mac_sharp),hoverColor: Colors.red,),
+        ListTile(title: Text('Hp'),leading: Icon(Icons.laptop_mac_sharp)),
+      ],
+    );
   }
 }
 
